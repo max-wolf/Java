791cfbc20babb3bc32fc184f8058ff74aa7aac9a
max-wolf <zyfwol@hotmail.com>
2017-12-16 19:39:51 +0800

    reflectionDemo

--- LambdaDemo/src/methodreference/Demo.java
+++ LambdaDemo/src/methodreference/Demo.java
@@ -1,22 +1,22 @@
 package methodreference;
 
 import java.time.LocalDate;
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.*;
+import java.util.function.Supplier;
 
 public class Demo {
     public static void main(String[] args) {
-        Person[] people = new Person[] {
+        Person[] people = new Person[]{
                 new Person("Clark", LocalDate.of(2003, 3, 12)),
                 new Person("Dephi", LocalDate.of(2004, 5, 4)),
                 new Person("Alice", LocalDate.of(2001, 9, 1)),
                 new Person("Bob", LocalDate.of(2003, 2, 25)),
         };
 
-        //lambda
+        /*lambda*/
         //Arrays.sort(people, (a, b) -> Person.compareByAge(a, b));
 
-        //Method Reference
+        /*Method Reference*/
         //Reference to a Static Method
         //Arrays.sort(people, Person::compareByAge);
 
@@ -24,12 +24,39 @@ public class Demo {
         //ComparisonProvider cp = new ComparisonProvider();
         //Arrays.sort(people, cp::compareByAge);
 
-        String[] stringArray = { "Barbara", "James", "Mary", "John", "Patricia", "Robert", "Michael", "Linda" };
-        //Arrays.sort(stringArray, String::compareToIgnoreCase);
-        Arrays.sort(stringArray, (a, b) -> a.compareToIgnoreCase(b));
+        System.out.println(Arrays.asList(people));
+
+        //Reference to an Instance Method of an Arbitrary Object of a Particular Type
+        String[] stringArray = {"Barbara", "James", "Mary", "John", "Patricia", "Robert", "Michael", "Linda"};
+        Arrays.sort(stringArray, String::compareToIgnoreCase);
         System.out.println(Arrays.asList(stringArray));
 
-        System.out.println(Arrays.asList(people));
+        Set<Person> sp = new HashSet<Person>(){
+            {
+                add(new Person("Clark", LocalDate.of(2003, 3, 12)));
+                add(new Person("Dephi", LocalDate.of(2004, 5, 4)));
+                add(new Person("Alice", LocalDate.of(2001, 9, 1)));
+                add(new Person("Bob", LocalDate.of(2003, 2, 25)));
+                add(new Person("John", LocalDate.of(2003, 2, 25)));
+            }
+        };
+        class A<T extends Person> extends HashSet<T> {
+            public A () {
+                add(new Person("Clark", LocalDate.of(2003, 3, 12)));
+            }
+        };
+        Set<Person> roster = new A<>();
+        Set<Person> set = transferElements(sp, HashSet<Person>::new);
+        System.out.println(set);
+    }
+
+    public static <T, SOURCE extends Collection<T>, DEST extends Collection<T>> DEST transferElements(
+            SOURCE sourceCollection, Supplier<DEST> collectionFactory) {
+        DEST result = collectionFactory.get();
+        for (T t : sourceCollection) {
+            result.add(t);
+        }
+        return result;
     }
 }
 
